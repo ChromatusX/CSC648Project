aa40d1220759362ad5b2b89e9769c22077d73d6e
camper4834 <camper4834@gmail.com>
2020-11-19 12:58:29 -0800

    Fixed the sql injection in db/item.js

--- application/db/item.js
+++ application/db/item.js
@@ -53,8 +53,8 @@ function newMessage(sender_id, item_id, message){
 function insertItem(name, description, price, category, fileName, userID){
     return new Promise(((resolve, reject) => {
         let baseSQL = `INSERT INTO item (name, description, price, category_name, image, user_id) 
-                  VALUES ('${name}', '${description}', '${price}', '${category}', '${fileName}', '${userID}')`
-        item.query(baseSQL)
+                  VALUES ('?', '?', '?', '?', '?', '?')`
+        item.query(baseSQL, [name, description, price, category, fileName, userID])
             .then((myPromise) =>{
                 resolve(myPromise)
             })
